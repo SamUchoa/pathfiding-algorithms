c3ff29f9cd1da4652465b261199c53ab6f6a1c3a
SamUchoa <samueluchoa088@gmail.com>
2025-11-10 18:42:30 -0300

    classe de grafo e testes

--- graphs.py
+++ graphs.py
@@ -1,9 +1,36 @@
 import numpy as np
 
 class Graph:
-    def __init__(self, adjacency_matrix: np.ndarray):
+    def __init__(self, adjacency_matrix: np.ndarray, directed: bool = False):
         self.adjacency_matrix = adjacency_matrix
+        self.vertices_number = adjacency_matrix.ndim
+        self.edges_number = np.count_nonzero(adjacency_matrix)
+        self.directed = directed
 
-    def adjacency_matrix(self):
-        return self.adjacency_matrix
+    def add_vertex(self):
+        new_line = np.array([0] * self.vertices_number)
+        new_column = np.array([[0]] * (self.vertices_number + 1))
 
+        self.adjacency_matrix = np.vstack((self.adjacency_matrix, new_line))
+        self.adjacency_matrix = np.hstack((self.adjacency_matrix, new_column))
+
+        self.vertices_number += 1
+        
+
+    def __str__(self):
+        return self.adjacency_matrix.__str__()
+    
+
+if __name__ == "__main__":
+    matrix = np.array([[0,1],[1,0]])
+    graph = Graph(matrix)
+    print("Matrix representation:")
+    print(graph, "\n")
+    print("Number of vertices:", graph.vertices_number)
+    print("Number of edges:", graph.edges_number)
+    
+    graph.add_vertex()
+    print("Matrix representation:")
+    print(graph, "\n")
+    print("Number of vertices:", graph.vertices_number)
+    print("Number of edges:", graph.edges_number)
