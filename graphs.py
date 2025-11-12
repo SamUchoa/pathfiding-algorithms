6e161662aee42dd0091259257a1f3ea1ac7f5b8d
SamUchoa <samueluchoa088@gmail.com>
2025-11-12 15:19:21 -0300

    metodos para remoção e adição de arestas e metodo para remoção de vertices

--- graphs.py
+++ graphs.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 class Graph:
-    def __init__(self, adjacency_matrix: np.ndarray, directed: bool = False):
+    def __init__(self, adjacency_matrix: np.ndarray):
         self.adjacency_matrix = adjacency_matrix
         self.vertices_number = adjacency_matrix.shape[0]
         self.edges_number = np.count_nonzero(adjacency_matrix)
-        self.directed = directed
 
     def add_vertex(self):
         new_line = np.array([0] * self.vertices_number)
@@ -15,7 +14,18 @@ class Graph:
         self.adjacency_matrix = np.hstack((self.adjacency_matrix, new_column))
 
         self.vertices_number += 1
-        
+
+    def remove_vertex(self, vertex: int): 
+        self.adjacency_matrix = np.delete(self.adjacency_matrix, vertex, axis=0)
+        self.adjacency_matrix = np.delete(self.adjacency_matrix, vertex, axis=1)
+
+    def add_edge(self, start_vertex: int, end_vertex: int):
+        self.adjacency_matrix[start_vertex,end_vertex] = 1
+        self.adjacency_matrix[end_vertex,start_vertex] = 1
+
+    def remove_edge(self, start_vertex: int, end_vertex: int):
+        self.adjacency_matrix[start_vertex,end_vertex] = 0
+        self.adjacency_matrix[end_vertex,start_vertex] = 0
 
     def __str__(self):
         return self.adjacency_matrix.__str__()
